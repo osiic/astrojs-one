f9eee6d141bd304c81a5c91a6528f0939503f6fc
Osi ic <96474947+osiic@users.noreply.github.com>
2023-11-05 01:01:57 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-import node from '@astrojs/node';
+import netlify from '@astrojs/netlify/functions';
 
 import sitemap from "@astrojs/sitemap";
 
@@ -9,7 +9,5 @@ export default defineConfig({
   site: "https://example.com",
   integrations: [mdx(), sitemap()],
   output: "server",
-  adapter: node({
-    mode: 'standalone',
-  })
+  adapter: netlify()
 });
